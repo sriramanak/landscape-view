3d3acc973576a4863f320eb2a251a8c723189afa
paradigm creatives <paradigm@paradigms-Mac-mini.local>
2014-09-30 11:06:14 +0530

    landscape view

---
+++ LandscapeSample/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  LandscapeSample
+//
+//  Created by paradigm creatives on 9/30/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ LandscapeSample/MyViewController.m
@@ -0,0 +1,43 @@
+//
+//  MyViewController.m
+//  LandscapeSample
+//
+//  Created by paradigm creatives on 9/30/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+
+@end
+
+@implementation MyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskLandscape;
+}
+
+@end
