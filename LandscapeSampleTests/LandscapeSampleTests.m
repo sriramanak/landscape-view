dae9d1dff2039ca2918f9c9fa84df68198729ea5
paradigm creatives <paradigm@paradigms-Mac-mini.local>
2014-09-30 10:14:28 +0530

    Initial Commit

---
+++ LandscapeSampleTests/LandscapeSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  LandscapeSampleTests.m
+//  LandscapeSampleTests
+//
+//  Created by paradigm creatives on 9/30/14.
+//  Copyright (c) 2014 Paradigm Creatives. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LandscapeSampleTests : XCTestCase
+
+@end
+
+@implementation LandscapeSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
